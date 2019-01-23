f649337a53dbca542cee7ad8060cc3a72efa459e
icomanman <fred_barabs@yahoo.com>
2019-01-24 01:11:51 +0800

    almost done?

--- src/App.js
+++ src/App.js
@@ -2,13 +2,38 @@ import React, { Component } from 'react';
 import './App.css';
 
 // Component Parts:
+import Particles from 'react-particles-js';
 import Nav from './parts/nav/nav.js';
 import Logo from './parts/logo/logo.js';
+
+const particles_options = {
+	particles: {
+		number: {
+			value: 250,
+			density: {
+				enable: true,
+				value_area: 400
+			}
+		}
+	},
+	interactivity: {
+		detect_on: 'canvas',
+		events: {
+			onhover: {
+				enable: true,
+				mode: 'repulse'
+			}
+		}
+	}
+};
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
+				<div className="particles">
+					<Particles params={particles_options} />
+				</div>
 				<Nav />
 				<Logo />
 			</div>
--- src/parts/logo/logo.js
+++ src/parts/logo/logo.js
@@ -6,16 +6,18 @@ import logo_file from './logo-pill.png';
 
 const Logo = () => {
 	return (
-		<div>
-			<Tilt className="Tilt" options={{ max: 45 }} style={{ height: 75, width: 75 }}>
-				<div className="Tilt-inner">
-					<img style={{ paddingTop: '15px' }} alt="logo.png" src={logo_file} />
-					<h3>HangTok</h3>
+		<div className="ma4 mt0">
+			<Tilt className="Tilt br2 shadow-2" options={{ max: 45 }} style={{ height: 120, width: 120 }}>
+				<div className="Tilt-inner pa3">
+					<img alt="logo.png" src={logo_file} />
 				</div>
+				<h3>HangTok</h3>
 			</Tilt>
 			<h4>Have you daily dose</h4>
 		</div>
 	);
 };
+
+// not yet pushed: 22 Jan 2019
 
 export default Logo;
